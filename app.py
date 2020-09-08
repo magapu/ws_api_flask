0143f0d38a793f62e8b55b30326a176ca13877ce
Srinivas <mssrinivas7@gmail.com>
2020-09-08 11:50:05 -0700

    pushing db changes

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-
+import yaml
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+yml = yaml.load(open("db.yaml"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pass@localhost:1995/sridb'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
