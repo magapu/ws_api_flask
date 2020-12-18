4d04de09b3f3c09fa66e2ef15cf0834d6d4f05cd
Srinivas Magapu <38003747+magapu@users.noreply.github.com>
2020-12-19 00:47:30 +0530

    Update app.py
    Added  predifined packages

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, request, json
 from flask_pymongo import PyMongo
-from Constants import UrlConstants as cons
-from Services import FetchAllRecordsService as fetchService
-from Services import CrudService as crudService
-from Services import EncryptService as encryptService
-from flask_cors import cross_origin
-from Services import LoginService as loginService
-from Services import DupCheckForEmailId as dupCheck
-from Services import ElasticSearchService as elasticSearch
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def home_page():
     return 'working'
 
