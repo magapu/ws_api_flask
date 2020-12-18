c99bf0c8034821e739631fcecb660bc0986d9060
Srinivas Magapu <38003747+magapu@users.noreply.github.com>
2020-12-19 00:25:55 +0530

    Update app.py
    Added controller

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home_page():
     return 'working'
 
+@app.route('/search/<string:_query>', methods=[constants.GET])
+@cross_origin()
+def search(_query):
+    return elasticSearchService.search_data(_query)
 
 if __name__ == '__main__':
     app.run()
