7db28017b68ad6202b67d9bf31e74ee12ce4ad5b
Srinivas Magapu <38003747+magapu@users.noreply.github.com>
2020-12-19 00:39:13 +0530

    Update app.py
    changed to basic

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 from flask import Flask, request, json
-from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
     return 'working'
-
-swagger_uri = '/swagger'
-api_uri = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    swagger_uri,
-    api_uri,
-    config={
-        'app_name': "Srinivas-flask-api"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=swagger_uri)
-
-
 
 if __name__ == '__main__':
     app.run()
